2f258a552d20c9c4972b24b8915ad047d4f5b9de
Zackary Chapple <zackarychapple30@gmail.com>
2016-10-23 22:26:47 -0400

    bug(fixing dom2page binding): further testing on dom2page

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ if (process.argv[ 2 ] === undefined) {
 }
 
 const html = `
-<html lang="en"><head>
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -67,7 +67,7 @@ const html = `
   
 
 
-</body></html>
+</body>
 `;
 
 const letterPortriateWidth = '1280';
